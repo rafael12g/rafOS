fa43ca1c9086da430d2d6d019d0ccabfa737d97e
rafael12g <0565rara12@gmail.com>
2025-10-25 23:21:12 +0000

    Delete kernelV0.1 directory

--- kernelV0.1/kernel/entry.asm
+++
@@ -1,7 +0,0 @@
-[bits 32]
-[extern kernel_main]        ; Déclaré dans kernel.c
-
-global _start
-_start:
-    call kernel_main
-    jmp $                   ; Boucle infinie si retour
--- kernelV0.1/libc/mem.h
+++
@@ -1,7 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-void memory_copy(char *source, char *dest, int nbytes);
-void memory_set(char *dest, char val, int len);
-
-#endif
