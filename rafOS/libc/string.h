7a925f3c8ce181b836ad455b0eb552a7cd4af921
rafael12g <0565rara12@gmail.com>
2025-10-26 00:48:30 +0000

    Delete rafOS directory

--- rafOS/libc/string.h
+++
@@ -1,9 +0,0 @@
-#ifndef STRING_H
-#define STRING_H
-
-int strcmp(const char* s1, const char* s2);
-int strlen(const char* str);
-void strcpy(char* dest, const char* src);
-int strncmp(const char* s1, const char* s2, int n);
-
-#endif
